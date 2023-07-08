a3df11add9b1301839267ee430c8e4b7d79dc4ff
Sam Poder <sam.r.poder@gmail.com>
2023-07-07 21:34:40 -0400

    Revert "create pdf"
    This reverts commit 615404bd61621dedcf7c7f0208633b0bcbcb0949.

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import chromium from "chrome-aws-lambda";
 
 export default async function handler(req, res) {
-  const { url } = req.query;
+  const { url, as } = req.query;
   const browser = await chromium.puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
@@ -13,14 +13,21 @@ export default async function handler(req, res) {
 
   let page = await browser.newPage();
 
-  await page.goto(url, { waitUntil: "networkidle0" });
+  await page.goto(url, { waitUntil: "networkidle2" });
 
-  const pdf = await page.pdf({ format: "A4" });
-
-  await browser?.close();
-
-  res.setHeader("Content-Type", "application/pdf");
-  res.setHeader("Accept-Ranges", "bytes");
-  res.setHeader("Content-Disposition", `inline; filename=file.pdf`);
-  res.send(pdf);
+  if (as === "pdf") {
+    const pdf = await page.pdf({ format: "A4" });
+    await browser?.close();
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Accept-Ranges", "bytes");
+    res.setHeader("Content-Disposition", `inline; filename=screenshot.pdf`);
+    res.send(imageBuffer);
+  } else {
+    const imageBuffer = await page.screenshot();
+    await browser?.close();
+    res.setHeader("Content-Type", "image/png");
+    res.setHeader("Accept-Ranges", "bytes");
+    res.setHeader("Content-Disposition", `inline; filename=screenshot.png`);
+    res.send(imageBuffer);
+  }
 }
